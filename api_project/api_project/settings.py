d2279f4b8d75ffe9d39a67df07d5a3ee0902fac4
darpanhh <darpangiri5@gmail.com>
2026-02-22 21:13:46 +0545

    Connect backend with frontend

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,16 +39,25 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'demo',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    # "https://yourfrontenddomain.com",
+    # "http://localhost:3000", 
+    # "http://127.0.0.1:8000",
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'api_project.urls'
